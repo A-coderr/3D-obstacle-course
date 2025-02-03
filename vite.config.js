3927bbc48b70b34c4155b61bfb3e094ac8deb596
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2025-02-03 17:28:19 -0500

    Removed unused dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
   assetsInclude: ["**/*.hdr", "**/*.glb", "**/*.obj"],
+  base: "./",
 });
